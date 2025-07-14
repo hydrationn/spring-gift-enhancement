5454ee7917098767eb0fa493b6c87c9e5c976404
hydrationn <146078205+hydrationn@users.noreply.github.com>
2025-07-15 00:21:55 +0900

    [Step 0] 기본 코드 준비 (#23)
    * chore: 위시 리스트 step3 코드 이관
    
    * docs: step0 명세 작성

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+create table product
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price BIGINT NOT NULL,
+    image_url VARCHAR(255)
+);
+
+CREATE TABLE member
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50)  NOT NULL
+);
+
+CREATE TABLE wish
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL
+);
