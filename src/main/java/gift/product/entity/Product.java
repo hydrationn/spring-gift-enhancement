b21bd18a6df555598537d54382daa79a98075146
hydrationn <suhwaparkk1031@gmail.com>
2025-07-18 03:38:32 +0900

    style: Product id 줄바꿈을 통한 가독성 개선

--- src/main/java/gift/product/entity/Product.java
+++ src/main/java/gift/product/entity/Product.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
 
     @Column(nullable = false)
